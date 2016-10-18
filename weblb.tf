e9fdb0f179367e1b444cb52e5e8f96143f183807
Zachary Gershman <zgershman@pivotal.io>
2016-10-18 15:44:00 -0700

    be more explicit about the rules
    - believe the rules cause the conflict on update
    make it so the proceeding rules have been created before
    attempting to make another one
    
    [#132341947]

--- weblb.tf
+++ weblb.tf
@@ -35,7 +35,7 @@ resource "azurerm_lb_probe" "web-https-probe" {
 }
 
 resource "azurerm_lb_rule" "web-https-rule" {
-  depends_on          = ["azurerm_lb.web"]
+  depends_on          = ["azurerm_lb.web", "azurerm_lb_probe.web-https-probe"]
   name                = "web-https-rule"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
@@ -62,6 +62,7 @@ resource "azurerm_lb_probe" "web-http-probe" {
 }
 
 resource "azurerm_lb_rule" "web-http-rule" {
+  depends_on          = ["azurerm_lb.web", "azurerm_lb_probe.web-http-probe", "azurerm_lb_rule.web-https-rule"]
   depends_on          = ["azurerm_lb.web"]
   name                = "web-http-rule"
   location            = "${var.location}"
@@ -79,7 +80,7 @@ resource "azurerm_lb_rule" "web-http-rule" {
 }
 
 resource "azurerm_lb_probe" "web-ssh-probe" {
-  depends_on          = ["azurerm_lb.web"]
+  depends_on          = ["azurerm_lb.web", "azurerm_lb_probe.web-https-probe"]
   name                = "web-ssh-probe"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
@@ -89,7 +90,7 @@ resource "azurerm_lb_probe" "web-ssh-probe" {
 }
 
 resource "azurerm_lb_rule" "web-ssh-rule" {
-  depends_on          = ["azurerm_lb.web"]
+  depends_on          = ["azurerm_lb.web", "azurerm_lb_probe.web-ssh-probe", "azurerm_lb_rule.web-http-rule", "azurerm_lb_rule.web-https-rule"]
   name                = "web-ssh-rule"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
