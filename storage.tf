dbf426d88e068ada77a525bf259f1f8ce158d22e
Dan Wendorf <dwendorf@pivotal.io>
2016-11-11 13:46:57 -0800

    Reduce template duplication by using `count`
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

--- storage.tf
+++ storage.tf
@@ -50,71 +50,31 @@ resource "azurerm_storage_table" "stemcells_storage_table" {
   storage_account_name = "${azurerm_storage_account.bosh_root_storage_account.name}"
 }
 
-resource "azurerm_storage_account" "bosh_vms_storage_account_1" {
-  name                = "${var.env_short_name}${data.template_file.base_storage_account_wildcard.rendered}1"
+resource "azurerm_storage_account" "bosh_vms_storage_account" {
+  name                = "${var.env_short_name}${data.template_file.base_storage_account_wildcard.rendered}${count.index}"
   resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
   location            = "${var.location}"
   account_type        = "Premium_LRS"
+
+  count = 3
 }
 
-resource "azurerm_storage_container" "bosh_storage_container_1" {
+resource "azurerm_storage_container" "bosh_vms_storage_container" {
   name                  = "bosh"
-  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account_1"]
+  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account"]
   resource_group_name   = "${azurerm_resource_group.pcf_resource_group.name}"
-  storage_account_name  = "${azurerm_storage_account.bosh_vms_storage_account_1.name}"
+  storage_account_name  = "${element(azurerm_storage_account.bosh_vms_storage_account.*.name, count.index)}"
   container_access_type = "private"
+
+  count = 3
 }
 
-resource "azurerm_storage_container" "stemcell_storage_container_1" {
+resource "azurerm_storage_container" "bosh_vms_stemcell_storage_container" {
   name                  = "stemcell"
-  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account_1"]
+  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account"]
   resource_group_name   = "${azurerm_resource_group.pcf_resource_group.name}"
-  storage_account_name  = "${azurerm_storage_account.bosh_vms_storage_account_1.name}"
+  storage_account_name  = "${element(azurerm_storage_account.bosh_vms_storage_account.*.name, count.index)}"
   container_access_type = "private"
-}
 
-resource "azurerm_storage_account" "bosh_vms_storage_account_2" {
-  name                = "${var.env_short_name}${data.template_file.base_storage_account_wildcard.rendered}2"
-  resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
-  location            = "${var.location}"
-  account_type        = "Premium_LRS"
-}
-
-resource "azurerm_storage_container" "bosh_storage_container_2" {
-  name                  = "bosh"
-  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account_2"]
-  resource_group_name   = "${azurerm_resource_group.pcf_resource_group.name}"
-  storage_account_name  = "${azurerm_storage_account.bosh_vms_storage_account_2.name}"
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_container" "stemcell_storage_container_2" {
-  name                  = "stemcell"
-  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account_2"]
-  resource_group_name   = "${azurerm_resource_group.pcf_resource_group.name}"
-  storage_account_name  = "${azurerm_storage_account.bosh_vms_storage_account_2.name}"
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_account" "bosh_vms_storage_account_3" {
-  name                = "${var.env_short_name}${data.template_file.base_storage_account_wildcard.rendered}3"
-  resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
-  location            = "${var.location}"
-  account_type        = "Premium_LRS"
-}
-
-resource "azurerm_storage_container" "bosh_storage_container_3" {
-  name                  = "bosh"
-  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account_3"]
-  resource_group_name   = "${azurerm_resource_group.pcf_resource_group.name}"
-  storage_account_name  = "${azurerm_storage_account.bosh_vms_storage_account_3.name}"
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_container" "stemcell_storage_container_3" {
-  name                  = "stemcell"
-  depends_on            = ["azurerm_storage_account.bosh_vms_storage_account_3"]
-  resource_group_name   = "${azurerm_resource_group.pcf_resource_group.name}"
-  storage_account_name  = "${azurerm_storage_account.bosh_vms_storage_account_3.name}"
-  container_access_type = "private"
+  count = 3
 }
