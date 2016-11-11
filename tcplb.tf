6c325e5cf6c88d9a74f166f6e1eed2a7795bdf7a
Dan Wendorf <dwendorf@pivotal.io>
2016-11-11 12:17:29 -0800

    Remove workaround to calculate some resource IDs
    - Backend Address Pools
    - Web HTTP Probes
    
    [#133226477]

--- tcplb.tf
+++ tcplb.tf
@@ -44,7 +44,6 @@ resource "azurerm_lb_rule" "tcp-rule" {
   frontend_port                  = "${count.index + 1024}"
   backend_port                   = "${count.index + 1024}"
 
-  # Workaround until the backend_address_pool and probe resources output their own ids
-  backend_address_pool_id = "${azurerm_lb.tcp.id}/backendAddressPools/${azurerm_lb_backend_address_pool.tcp-backend-pool.name}"
-  probe_id                = "${azurerm_lb.tcp.id}/probes/${azurerm_lb_probe.tcp-probe.name}"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.tcp-backend-pool.id}"
+  probe_id                = "${azurerm_lb_probe.tcp-probe.id}"
 }
