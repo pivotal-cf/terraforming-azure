4f07de9c2593fb0bd906bcf71ed3df25a2d33af8
Dave Walter <dwalter@pivotal.io>
2016-12-20 15:49:39 -0800

    Rename opsman-18.vhd to opsman.vhd
    [#135823695]
    
    Signed-off-by: Aditya Anchuri <aanchuri@pivotal.io>

--- storage.tf
+++ storage.tf
@@ -21,7 +21,7 @@ resource "azurerm_storage_container" "ops_manager_storage_container" {
 }
 
 resource "azurerm_storage_blob" "ops_manager_image" {
-  name                   = "opsman-18.vhd"
+  name                   = "opsman.vhd"
   resource_group_name    = "${azurerm_resource_group.pcf_resource_group.name}"
   storage_account_name   = "${azurerm_storage_account.ops_manager_storage_account.name}"
   storage_container_name = "${azurerm_storage_container.ops_manager_storage_container.name}"
