747683566de7bc8b756dd8460a3efa78c1f9c741
Kalai Wei <kawei@pivotal.io>
2017-11-16 14:58:54 -0800

    Rename ert_subnet to pas_subnet for consistency
    - mirrors the change to terraforming-gcp
    
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

--- mysqllb.tf
+++ mysqllb.tf
@@ -5,7 +5,7 @@ resource "azurerm_lb" "mysql" {
 
   frontend_ip_configuration = {
     name      = "frontendip"
-    subnet_id = "${azurerm_subnet.ert_subnet.id}"
+    subnet_id = "${azurerm_subnet.pas_subnet.id}"
   }
 }
 
