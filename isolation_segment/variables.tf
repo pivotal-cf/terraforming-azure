257335adb64b7cfc6099358ace49396e5e62de8b
Lyle Franklin <lfranklin@pivotal.io>
2017-12-20 11:42:24 -0800

    Add optional ssl cert properties
    - Adds ssl_cert/private_key inputs and ouputs
      - If specifed, these keys are passed thru as an output
      - The user can then pass this output to the gorouter BOSH manifest
    - If ssl_ca_cert/private_key is provided, the templates will generate a
      self-signed SSL cert with hostnames for `*.apps.DOMAIN` and `*.sys.DOMAIN`
    - Adds similar properties for iso seg module
    
    [#151189254]

--- isolation_segment/variables.tf
+++ isolation_segment/variables.tf
@@ -15,3 +15,27 @@ variable "resource_group_name" {
 variable "dns_zone" {
   type = "string"
 }
+
+variable "ssl_cert" {
+  type        = "string"
+  description = "the contents of an SSL certificate which should be passed to the isoseg gorouter, optional if `ssl_ca_cert` is provided"
+  default     = ""
+}
+
+variable "ssl_private_key" {
+  type        = "string"
+  description = "the contents of an SSL private key which should be passed to the isoseg gorouter, optional if `ssl_ca_cert` is provided"
+  default     = ""
+}
+
+variable "ssl_ca_cert" {
+  type        = "string"
+  description = "the contents of a CA public key to be used to sign a generated certificate for isoseg gorouter, optional if `ssl_cert` is provided"
+  default     = ""
+}
+
+variable "ssl_ca_private_key" {
+  type        = "string"
+  description = "the contents of a CA private key to be used to sign a generated certificate for isoseg gorouter, optional if `ssl_cert` is provided"
+  default     = ""
+}
