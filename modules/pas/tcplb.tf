3ea2b49b6fa0f7f9841ccf65b521dd1a09373e7a
Carlos Iriarte <me@carlosiriarte.com>
2018-10-25 14:33:14 -0700

    Add Firewall Rule to Allow UDP 123 for NTP on TCP Router
    [#159189782](https://www.pivotaltracker.com/story/show/159189782)
    Co-authored-by: Christopher Hunter <hunter.christopher@icloud.com>
    Co-authored-by: Chris Dutra <cdutra@pivotal.io>

--- modules/pas/tcplb.tf
+++ modules/pas/tcplb.tf
@@ -46,3 +46,16 @@ resource "azurerm_lb_rule" "tcp-rule" {
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.tcp-backend-pool.id}"
   probe_id                = "${azurerm_lb_probe.tcp-probe.id}"
 }
+
+resource "azurerm_lb_rule" "ntp" {
+  name                = "ntp-rule"
+  resource_group_name = "${var.resource_group_name}"
+  loadbalancer_id     = "${azurerm_lb.tcp.id}"
+
+  frontend_ip_configuration_name = "frontendip"
+  protocol                       = "UDP"
+  frontend_port                  = "123"
+  backend_port                   = "123"
+
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.tcp-backend-pool.id}"
+}
