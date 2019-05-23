af60f78ee28f14bd6340fa0908b9af2aa796d07d
Bryan Kelly <xyloman@gmail.com>
2019-05-22 22:14:05 -0500

    Update outputs.tf

--- terraforming-pas/outputs.tf
+++ terraforming-pas/outputs.tf
@@ -149,7 +149,8 @@ output "ops_manager_storage_account" {
 }
 
 output "ops_manager_storage_account_access_key" {
-  value = "${module.ops_manager.ops_manager_storage_account_access_key}"
+  sensitive = true
+  value     = "${module.ops_manager.ops_manager_storage_account_access_key}"
 }
 
 output "cf_storage_account_name" {
