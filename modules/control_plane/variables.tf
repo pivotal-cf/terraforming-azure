c0dfc27c4832b0182555e89f6d61a5332281c6e0
Nick Rohn <nrohn@pivotal.io>
2019-05-30 10:04:45 -0700

    Removes control_plane module
    [#166221043]

--- modules/control_plane/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cidr" {}
-variable "dns_zone_name" {}
-variable "env_name" {}
-variable "external_db" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "network_name" {}
-variable "postgres_username" {}
